ba20d45f1816d82cf9e6bbad7e419d52b6a6a924
Charles Ybarra <charles.ybarra@outlook.com>
2021-04-27 15:50:51 -0500

    added prac f(x) for binary

--- searchingAlgos.js
+++ searchingAlgos.js
@@ -1,11 +1,11 @@
 //Binary Search Algo
 const list = [3, 5, 6, 8, 11, 12, 14, 15, 16, 17, 18];
-const item = 8;
+const item = 15;
 
 //recursive vs iterative
 
 //iterative
-function binarySearch2(){
+function binarySearch(){
     let low = 0
     let high = list.length - 1
 
@@ -32,6 +32,38 @@ function binarySearch2(){
         }
     }
     return null
+}
+
+function binarySearch2(){
+    //define low = 0 
+    let low = 0
+    //define high = list.length -1
+    let high = list.length - 1 
+
+    //have a while condition; while low <= high start searching
+    while(low <= high){
+
+        //mid and guess need to be in while condition
+        //ISSUE WAS parantheses for mid calculation
+        //define mid index = Math.floor(low + high / 2 )
+        let mid = Math.floor( (low + high) / 2)
+        let guess = list[mid]
+
+        //if guess == item, return array position of guess
+        if(guess === item){
+            console.log(`Guess value is ${list[mid]};`, `guess found at index position ${mid}`)
+            return
+        }
+        console.log("iterating now", `guess is ${guess}`)
+        if (guess > item){
+            high = mid -1
+        }
+
+        else{
+            low = mid + 1 
+        }
+    }
+    return -1
 }
 
 binarySearch2(list, item)
